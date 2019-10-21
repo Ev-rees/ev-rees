2890aa44deb18360f7a792d223d3452be82e0c3c
Los Mammoths <evrees.chaosmedia@gmail.com>
2019-10-21 00:07:38 -0400

    Finit un story
    - Le personnage peut courir sur un plateau

---
+++ Projet Unity/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Runtime.Serialization;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    private float speedZ = 300.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, speedZ * Time.deltaTime);
+    }
+}
---
+++ Projet Unity/Assets/Scripts/TileManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileManager : MonoBehaviour
+{
+
+    public GameObject[] tilePrefabs;
+    private Transform player;
+    private float spawnZ = -15.0f;
+    private float tileLength = 15.0f;
+    private float safeZone = 20.0f;
+
+    private int amountTilesOnScreen = 5;
+
+    private List<GameObject> activeTiles;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        activeTiles = new List<GameObject>();
+        player = GameObject.FindGameObjectWithTag("player").transform;
+
+        for(int i = 0; i < amountTilesOnScreen; i++)
+        {
+            SpawnTile();
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (player.position.z - safeZone > (spawnZ - amountTilesOnScreen * tileLength))
+        {
+            SpawnTile();
+            DeleteTile();
+        }
+    }
+
+    private void SpawnTile(int prefabIndex = -1)
+    {
+        GameObject go;
+        go = Instantiate(tilePrefabs[0]) as GameObject;
+        go.transform.SetParent(transform);
+
+        Debug.Log(Vector3.forward);
+
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ += tileLength;
+
+        activeTiles.Add(go);
+    }
+
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+}
