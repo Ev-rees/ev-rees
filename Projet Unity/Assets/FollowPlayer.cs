2fc146dfcbad31f34ecb9859e19c1768476ef83d
Los Mammoths <evrees.chaosmedia@gmail.com>
2019-10-20 21:29:47 -0400

    Premier push

---
+++ Projet Unity/Assets/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform with a small "t" refers to the current object in the class
+        transform.position = player.position + offset;
+    }
+}
---
+++ Projet Unity/Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 1000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movePlayer();
+    }
+
+    void movePlayer() {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+    }
+}
